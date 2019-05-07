f63c34ee9b375f36fee6f9bbfb1aa3c11e8f5d90
Axel <ax.fiolle@gmail.com>
2019-05-07 17:18:21 +0200

    imported react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './pages/home/main'
+
+import './app.css'
+
+export default class App extends React.Component {
+	render() {
+		return (
+			 <div className="App">
+					 <Router>
+							 <Switch>
+									 <Route path="/" component={Home} />
+							 </Switch>
+					 </Router>
+			 </div>
+		);
+	}
+}
